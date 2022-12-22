97e8c8bb0681dd453b247c4d687b562fde607569
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2022-12-22 02:51:56 +0000

    Update server.py

--- server.py
+++ server.py
@@ -3,15 +3,21 @@ from subprocess import call, Popen, PIPE
 import shlex
 import time
 
-cmd = "nc -lnvp 5000 >> results.txt"
+# Create an output file
+filename = "locations.txt"
+cmd = f"nc -lnvp 5000 >> {filename}"
 clear = "clear"
 
-filename = "results.txt"
+# Let it run indefinitely (use a cron job I recommend)
 while True:
+# This way the shellcode runs in the background
     Popen(shlex.split(cmd), stderr=PIPE, stdin=PIPE, stdout=PIPE)
+# Read the file
     with open(filename, "r") as thefile:
         contents = thefile.read()
+# Display contents onscreen
         print(contents)
+# Clear the screen every five seconds to keep things from getting too messy
         time.sleep(5)
         call(clear)
         continue
