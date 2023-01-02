a2f6b47572d618e7d372d2bf465e11a7c26e1cec
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2023-01-02 18:29:46 +0000

    Update and rename server.sh to server.py

---
+++ server.py
@@ -0,0 +1,28 @@
+#!/bin/python3.9
+import socket
+import sys
+
+s = socket.socket()
+s.bind(("127.0.0.1",9000)) #Change these
+print("Listening...")
+s.listen(10)
+i=1
+
+while True:
+    sc, address = s.accept()
+    print(address)
+    f = open('location.txt','ab+') #open in binary
+    l = 1
+    while(l):
+        l = sc.recv(1024)
+        while (l):
+            f.write(l)
+            l = sc.recv(1024)
+        f.close()
+        with open('location.txt', 'r') as thefile:
+            contents = thefile.read()
+            print(contents)
+
+    sc.close()
+
+s.close()
--- server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while :
-do
-    nc -lnvp 5000 >> location.txt
-    sleep 3
-done
